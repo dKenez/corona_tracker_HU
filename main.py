6f6d74b222c9ba2d892cd3fb03af859a05305bbb
Kenéz <47757462+dKenez@users.noreply.github.com>
2020-04-18 14:58:14 +0200

    Added os dependency
    Relative file paths did not work when run by script, implemented fix using os library.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import datetime
 import requests
+import os
 
 import CaseScraper
 import DeathScraper
 # import RegionScraper
-
 
 def get_date_time():
     now = datetime.datetime.now()
@@ -18,6 +18,11 @@ def connect_url(url):
     return source.text
 
 
+def get_abs_path(rel_path):
+    project_folder = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(project_folder, rel_path)
+
+
 URL_cases = "https://koronavirus.gov.hu/"
 URL_deaths = "https://koronavirus.gov.hu/elhunytak"
 URL_regions = "https://koronavirus.gov.hu/terkepek/fertozottek"
@@ -26,10 +31,15 @@ src_cases = connect_url(URL_cases)
 src_deaths = connect_url(URL_deaths)
 src_regions = connect_url(URL_regions)
 
-file_cases = "data/case_DATA.csv"
-file_deaths = "data/death_DATA.csv"
-file_conditions = "data/conditions_DATA.csv"
-file_regions = "data/region_DATA.csv"
+path_cases = "data/case_DATA.csv"
+path_deaths = "data/death_DATA.csv"
+path_conditions = "data/conditions_DATA.csv"
+path_regions = "data/region_DATA.csv"
+
+file_cases = get_abs_path(path_cases)
+file_deaths = get_abs_path(path_deaths)
+file_conditions = get_abs_path(path_conditions)
+file_regions = get_abs_path(path_regions)
 
 log_date, log_time = get_date_time()
 
@@ -40,8 +50,9 @@ update_date = case_scraper.date
 update_time = case_scraper.time
 del case_scraper
 
+
 death_scraper = DeathScraper.DeathScraper(file_deaths, file_conditions, src_deaths, log_date, log_time, update_date,
-                                          update_time, force_conditions_update=False)
+                                          update_time, force_conditions_update=True)
 page_count = death_scraper.get_page_count()
 for i in range(1, page_count+1):
     url_i = "https://koronavirus.gov.hu/elhunytak?page={0}".format(i)
